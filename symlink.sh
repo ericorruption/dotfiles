c507e06730491ddb8094c40073b83c9a4cfcf954
Eric Quanz <eric.quanz@gmail.com>
2015-03-31 15:34:32 -0300

    add config files to be symlinked

---
+++ symlink.sh
@@ -0,0 +1,12 @@
+# ZSH
+ln -s ~/Sites/dotfiles/.zshrc ~/.zshrc
+
+# Sublime Text
+ln -s ~/Sites/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s ~/Sites/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+# homebrew does this one already!
+# ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+# IOS SIM!
+sudo ln -s "/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app" "/Applications/iOS Simulator.app"
