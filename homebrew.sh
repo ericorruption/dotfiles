edfb91e91efb025bce64808311e5154adaad833e
Eric Quanz <eric.quanz@gmail.com>
2015-03-31 15:34:07 -0300

    update docs, add homebrew/cask script

---
+++ homebrew.sh
@@ -0,0 +1,44 @@
+# Run all updates that are possible from the App Store.
+xcode-select --install
+
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew doctor
+brew update
+brew upgrade
+brew install git node bash zsh tidy caskroom/cask/brew-cask
+brew cleanup
+
+# Apps
+apps=(
+  asepsis
+  caffeine
+  appcleaner
+  the-unarchiver
+  spectacle
+
+  google-chrome
+  firefoxdeveloperedition
+  opera
+  filezilla
+  vagrant
+  virtualbox
+  totalterminal
+  sublime-text3
+  sourcetree
+  xquartz
+
+  spotify
+  sonora
+
+  qlstephen
+  qlimagesize
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew tap caskroom/versions
+brew cask install --appdir="/Applications" ${apps[@]}
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-# Ericorruption's dotfiles
+# Ericorruption's onboarding files
+Just run these in order:
+1. defaults.sh
+2. homebrew.sh
+3. shell.sh
+4. npm.sh
+5. symlink.sh
+
+##TODO
+* Download IE VMs
