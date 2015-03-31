a4f4012606fc381dd2ba0a4489e394a784a09364
Eric Quanz <eric.quanz@gmail.com>
2015-03-31 15:33:26 -0300

    reorder and add defaults

--- defaults.sh
+++ defaults.sh
@@ -1,61 +1,75 @@
-# Wipe all (default) app icons from the Dock
-#defaults write com.apple.dock persistent-apps -array
+#!/usr/bin/env bash
 
+# Infinite sudo
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+###############################################################################
+# General UI/UX
+###############################################################################
+
+# Return from sleep faster
+sudo pmset -a standbydelay 86400
+# Disable hibernation
+sudo pmset -a hibernatemode 0
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+# Disable time machine
+sudo tmutil disablelocal
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
 
 # Hide the Time Machine, Volume, User, and Bluetooth icons
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-    defaults write "${domain}" dontAutoLoad -array \
-      "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-      "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-      "/System/Library/CoreServices/Menu Extras/Volume.menu" \
-      "/System/Library/CoreServices/Menu Extras/User.menu"
-  done
+    sudo defaults write "${domain}" dontAutoLoad -array \
+        "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+        "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+        "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+        "/System/Library/CoreServices/Menu Extras/User.menu"
+done
 
-  defaults write com.apple.systemuiserver menuExtras -array \
+defaults write com.apple.systemuiserver menuExtras -array \
     "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
     "/System/Library/CoreServices/Menu Extras/Battery.menu" \
     "/System/Library/CoreServices/Menu Extras/Clock.menu"
 
+
+# Increasing the window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
 # Save to disk instead of iCloud
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
+# Disable the "Are you sure you want to open this application?" dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# Disable hibernation
-sudo pmset -a hibernatemode 0
-
-# Return from sleep faster
-sudo pmset -a standbydelay 86400
-
-## VIEW STYLE
-defaults write com.apple.Finder FXPreferredViewStyle clmv
-
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-## SAFARI DEV EDITION
-defaults write com.apple.Safari ShowFavoritesBar -bool false
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-## SHOW LIBRARY FOLDER
-#chflags nohidden ~/Library
-
-# Disable menubar transparency
-defaults write com.apple.universalaccess reduceTransparency -bool true
+# Download updates by default
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate 'AutomaticDownload' -bool true
 
 
-defaults write com.apple.finder AppleShowAllFiles -bool true
-defaults write com.apple.finder AppleShowAllFiles TRUE
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+###############################################################################
+# Keyboard, mouse & input
+###############################################################################
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# Disabling auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write -g NSAllowContinuousSpellChecking -bool false
 
 
-#
-# SCREEN
-#
+###############################################################################
+# Screen
+###############################################################################
+
 #Requiring password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
@@ -63,51 +77,93 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 
-#
-# FINDER
-#
+###############################################################################
+# Finder
+###############################################################################
 
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+# Show Removable media on Desktop
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 # Disabling the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0;
 
-#
-# DOCK
-#
+## VIEW STYLE - column view
+defaults write com.apple.Finder FXPreferredViewStyle -string "clmv"
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+  General -bool true \
+  OpenWith -bool true \
+  Privileges -bool true
+
+
+###############################################################################
+# Dock / Dashboard
+###############################################################################
 
 # Setting the icon size of Dock items
-defaults write com.apple.dock tilesize -int 60
+defaults write com.apple.dock tilesize -int 64
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
 # Don't animate opening applications from the Dock
 defaults write com.apple.dock launchanim -bool false
-
-
-#
-# Others
-#
+defaults write com.apple.dock 'magnification' -bool false
+defaults write com.apple.dock 'mineffect' -string 'scale'
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
 
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
 # Don't automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
-# Increasing the window resize speed for Cocoa applications
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
-# Disabling auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-# Disable the "Are you sure you want to open this application?" dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-# Disable time machine
-hash tmutil &> /dev/null && sudo tmutil disablelocal
 
+# Wipe all (default) app icons from the Dock
+defaults write com.apple.dock persistent-apps -array
+
+
+###############################################################################
+# Taskbar
+###############################################################################
+
+# Menu bar: show battery percentage, not time remaining
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "NO"
+
+# 24-Hour Time
+defaults write NSGlobalDomain AppleICUForce12HourTime -bool false
+
+
+###############################################################################
+# Restart services
+###############################################################################
 
 killall Finder
 killall Dock
 killall cfprefsd
 killall SystemUIServer
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
