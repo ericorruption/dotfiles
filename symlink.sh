a0d2f3a7585f65c261902232df788cc6389fd918
Eric Quanz <eric.quanz@gmail.com>
2016-01-07 17:41:13 -0200

    remove sublime text symlinking, as well as iOS simulator symlink.
    In order to use brew to control the iOS simulator, I decided to remove the symlinking of the installation present in XCode.

--- symlink.sh
+++ symlink.sh
@@ -1,17 +1,3 @@
 # ZSH
 rm ~/.zshrc
 ln -s ~/Sites/dotfiles/.zshrc ~/.zshrc
-
-# Sublime Text
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-
-ln -s ~/Sites/dotfiles/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -s ~/Sites/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-
-# homebrew does this one already!
-# ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-# If you need to use this separately on the terminal remove the quotes
-
-# IOS SIM!
-sudo ln -s "/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app" "/Applications/iOS Simulator.app"
