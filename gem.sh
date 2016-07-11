a0a6753154fbd9d26535146911ca5d9742c1a2e1
Eric Quanz <eric.quanz@gmail.com>
2016-07-11 13:09:34 -0300

    add rbenv global config with some essential gems, symlink irbrc

--- gem.sh
+++ gem.sh
@@ -1,1 +1,8 @@
-gem install rails bundler
+echo 'eval "$(rbenv init -)"' >> ~/Sites/dotfiles/.zshrc
+sourcezsh
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+
+gem update
+gem install rails bundler awesome_print brakeman rails_best_practices
--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,4 @@
 # ZSH
 rm ~/.zshrc
 ln -s ~/Sites/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Sites/dotfilez/.irbrc ~/.irbrc
